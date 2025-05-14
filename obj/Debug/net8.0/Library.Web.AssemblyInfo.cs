0f84901378f68edad1fc0ba78ab3c47d5dfef23e
Tarek <tareknasr1994@gmail.com>
2025-05-14 12:42:53 +0300

    Modify book Views

--- obj/Debug/net8.0/Library.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/Library.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e31e92d4213d42711632a6dec0fd35146a8b4dec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a64ce8a9e29af40633b794b1feba452a217cfc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
